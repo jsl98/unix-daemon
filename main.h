39189d1eb306ef8c1b5cf4201dfc630d1da3da00
jsl98 <jsl98@nau.edu>
2015-04-09 10:22:55 -0700

    final main.h

--- main.h
+++ main.h
@@ -1,8 +1,13 @@
+/* Homework 7
+ * Author: Justin Liddicoat, Daniel Durazo, Tsosie Schneider
+ *	Last Modified:  9 April, 2015
+ */
+ 
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <errno.h>
-#include 
-#include
+#include <string.h>
 
 #include "daemon.h"
 
